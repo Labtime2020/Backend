62df8a473e2663ec1acf8df4b7d9851d5a1b47e0
rafaelcastro141 <rafaelgyncastro63@gmail.com>
2020-01-07 15:10:01 -0300

    metodo para buscar usuario pelo email, e classe para envio de resposta

---
+++ postgretest/src/main/java/com/example/postgretest/controller/Resposta.java
@@ -0,0 +1,11 @@
+package com.example.postgretest.Controller;
+
+public class Resposta{
+	public int code;
+	public String msg;
+
+	public Resposta(int code, String msg){
+		this.code = code;
+		this.msg = msg;
+	}
+}
